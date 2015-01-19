9cd16db67450961644c997ab15a06f6588e68c16
Camilo Ordonez <icoj@gmail.com>
2015-01-19 01:47:14 -0800

    Game complete - Includes Bonus

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
 
 var movement1 = document.getElementById("c1");
-movement1.addEventListener("click", function(){alternate(1);});
+movement1.addEventListener("click", function(){playTurn(1);});
 var movement2 = document.getElementById("c2");
-movement2.addEventListener("click", function(){alternate(2);});
+movement2.addEventListener("click", function(){playTurn(2);});
 var movement3 = document.getElementById("c3");
-movement3.addEventListener("click", function(){alternate(3);});
+movement3.addEventListener("click", function(){playTurn(3);});
 var movement4 = document.getElementById("c4");
-movement4.addEventListener("click", function(){alternate(4);});
+movement4.addEventListener("click", function(){playTurn(4);});
 var movement5 = document.getElementById("c5");
-movement5.addEventListener("click", function(){alternate(5);});
+movement5.addEventListener("click", function(){playTurn(5);});
 var movement6 = document.getElementById("c6");
-movement6.addEventListener("click", function(){alternate(6);});
+movement6.addEventListener("click", function(){playTurn(6);});
 var movement7 = document.getElementById("c7");
-movement7.addEventListener("click", function(){alternate(7);});
+movement7.addEventListener("click", function(){playTurn(7);});
 var movement8 = document.getElementById("c8");
-movement8.addEventListener("click", function(){alternate(8);});
+movement8.addEventListener("click", function(){playTurn(8);});
 var movement9 = document.getElementById("c9");
-movement9.addEventListener("click", function(){alternate(9);});
+movement9.addEventListener("click", function(){playTurn(9);});
 
 
 var Items = ["X","O","X","O","X","O","X","O","X"];
 var Item = " ";
 
 
-function alternate(cell){
+function playTurn(cell){
 	if (Items.length === 0){
 		setTimeout(function(){alert("Game Over");},300);
 	} else {
-		turnOfPlayer();		
+		turnOfPlayerMessage();		
 		element = "movement" + cell;
 		var movement = window[element];
 		if (movement.innerHTML === "") {
@@ -41,7 +41,7 @@ function alternate(cell){
 
 
 var contItem = 0;
-function turnOfPlayer (){
+function turnOfPlayerMessage (){
 	contItem++
 		var message = document.getElementById("messageText");
 		if (contItem % 2 === 0) {
@@ -51,15 +51,37 @@ function turnOfPlayer (){
 		}
 }
 
-var contWins = 0;
+
 function winnerMessage(line, number){
-	contWins++;
 	var message = document.getElementById("winner");
 	alert(message.innerHTML = "The winner is " + "\""+ Item +"\" --> " + line + ": " + number);
-	console.log(contWins);
-	Items = ["X","O","X","O","X","O","X","O","X"];
-	console.log(Items);	
+	
+	counterWins();
 }
+
+
+var xContWins = 0;
+var cContWins = 0;
+function counterWins (){
+	
+	if (Item === "X") {
+		xContWins++;
+		var message = document.getElementById("counter");
+		message.innerHTML = "Counter: " + xContWins;
+		cContWins = 0;	
+	} else {
+		cContWins++;
+		xContWins = 0;
+		var message = document.getElementById("counter");
+		message.innerHTML = "Counter: " + cContWins;
+		console.log(cContWins);
+	}	
+	if (cContWins === 3 || xContWins ===3) {
+		alert("CONGRATULATIONS!! 3 WINS IN A ROW!");
+	};
+	Items = [];
+}
+
 
 
 function changeColor(){
@@ -99,7 +121,7 @@ function resetGame (){
 		allCells[i].setAttribute("class","resetclass");
 		allCells[i].innerHTML = "";
 	};
-
+	Items = ["X","O","X","O","X","O","X","O","X"];
 	
 }
 
